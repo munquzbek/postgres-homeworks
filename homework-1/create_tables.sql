7af7c7e60383a83aba5a09252ee70f730218fc02
munquzbek <munchik31@gmail.com>
2023-09-29 23:01:40 -0400

    HW#1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,57 @@
 -- SQL-команды для создания таблиц
+
+--Open postgres in command lane
+--postgres=# \l
+--                                                       List of databases
+--   Name    |  Owner   | Encoding | Locale Provider |   Collate   |    Ctype    | ICU Locale | ICU Rules |   Access privileges
+-------------+----------+----------+-----------------+-------------+-------------+------------+-----------+-----------------------
+-- postgres  | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           |
+-- template0 | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           | =c/postgres          +
+--           |          |          |                 |             |             |            |           | postgres=CTc/postgres
+-- template1 | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           | =c/postgres          +
+--           |          |          |                 |             |             |            |           | postgres=CTc/postgres
+-- test      | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           |
+--(4 rows)
+--
+--postgres=# CREATE DATABASE north;
+--CREATE DATABASE
+--postgres=# \l
+--                                                       List of databases
+--   Name    |  Owner   | Encoding | Locale Provider |   Collate   |    Ctype    | ICU Locale | ICU Rules |   Access privileges
+-------------+----------+----------+-----------------+-------------+-------------+------------+-----------+-----------------------
+-- north     | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           |
+-- postgres  | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           |
+-- template0 | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           | =c/postgres          +
+--           |          |          |                 |             |             |            |           | postgres=CTc/postgres
+-- template1 | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           | =c/postgres          +
+--           |          |          |                 |             |             |            |           | postgres=CTc/postgres
+-- test      | postgres | UTF8     | icu             | en_US.UTF-8 | en_US.UTF-8 | en-US      |           |
+--(5 rows)
+--
+--postgres=#
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY CHECK (upper(customer_id) = customer_id),
+	company_name varchar(50),
+	contact_name varchar(100)
+);
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id) NOT NUll,
+	employee_id int REFERENCES employees(employee_id) NOT NUll,
+	order_date date,
+	ship_city varchar(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,58 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+customers_path = 'north_data/customers_data.csv'
+employees_path = 'north_data/employees_data.csv'
+orders_path = 'north_data/orders_data.csv'
+
+conn_params = {
+    'host': "localhost",
+    'database': "north",
+    'user': "postgres",
+    'password': "12345"
+}
+
+
+def get_data(path):
+    file = open(path, 'r')
+    csvfile = csv.DictReader(file)
+    return csvfile
+
+
+def customers_into_table():
+    with psycopg2.connect(**conn_params) as conn:
+        data = get_data(customers_path)
+        with conn.cursor() as cur:
+            for d in data:
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                            (d['customer_id'], d['company_name'], d['contact_name']))
+                cur.execute("SELECT * FROM employees")
+    conn.close()
+
+
+def employees_into_table():
+    with psycopg2.connect(**conn_params) as conn:
+        data = get_data(employees_path)
+        with conn.cursor() as cur:
+            for d in data:
+                cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                            (d['employee_id'], d['first_name'], d['last_name'], d['title'],
+                             d['birth_date'], d['notes']))
+    conn.close()
+
+
+def orders_into_table():
+    with psycopg2.connect(**conn_params) as conn:
+        data = get_data(orders_path)
+        with conn.cursor() as cur:
+            for d in data:
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                            (d['order_id'], d['customer_id'], d['employee_id'], d['order_date'],
+                             d['ship_city']))
+    conn.close()
+
+
+# orders_into_table()
+# employees_into_table()
+# customers_into_table()
