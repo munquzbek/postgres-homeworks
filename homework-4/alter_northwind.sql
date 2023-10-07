83b58477b6cf382e97c344f63e8593a26158fc42
munquzbek <munchik31@gmail.com>
2023-10-07 08:40:39 -0400

    HW#4

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,42 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
-
-
+ALTER TABLE products
+ADD CONSTRAINT chk_products_unit_price
+CHECK (unit_price >= 0)
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
-
-
+ALTER TABLE products
+ADD CONSTRAINT chk_products_discontinued
+CHECK (discontinued IN (0, 1))
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-
-
+SELECT * INTO new_table_products
+FROM products
+WHERE discontinued = 1
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+
+-- Way number 1:
+--create new table with deleted data
+--but in table order_details still will be all data(deleted and not deleted products)
+SELECT * INTO not_deleted_products
+FROM products
+WHERE discontinued = 0;
+
+SELECT *
+FROM not_deleted_products
+
+-- Way number 2:
+--in table order_details also delete data as in table products
+
+--drop already exist constraint
+ALTER TABLE order_details
+DROP CONSTRAINT fk_order_details_products;
+--add and update foreign key constraint
+ALTER TABLE order_details
+ADD CONSTRAINT fk_order_details_products
+FOREIGN KEY(product_id)
+REFERENCES products(product_id)
+ON DELETE CASCADE;
+--delete from main table
+DELETE FROM products
+WHERE discontinued = 1
+
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,33 @@
--- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
 
+
+-- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+CREATE TABLE student
+(
+	student_id serial,
+	first_name varchar(50),
+	last_name varchar(50),
+	birthday date,
+	phone varchar(50)
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student ADD COLUMN middle_name varchar(50);
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student RENAME birthday TO birth_day;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student
+ALTER COLUMN phone TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student(first_name, last_name, birthday, phone)
+VALUES ('MUNDUZBEK', 'BOOBEKOV', '2003,08,14', 8284831823),
+('IVAN', 'IVANOV', '2000,09,15', 8284831824),
+('PETR', 'PETROV', '2001,10,16', 8284831825);
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
